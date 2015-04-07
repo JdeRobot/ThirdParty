acc3962f86d9195d7c0d199996139acb040685f1
Roberto Calvo <rocapal@gmail.com>
2015-04-07 18:02:46 +0200

    updated stable version of OpenNI2

---
+++ OpenNI2/openni2-2.2.0/.pc/install11/Packaging/Linux/install-jderobot.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -e
+
+WORK_DIR=`pwd`
+
+echo "Installing in $DESTDIR"
+
+mkdir -p $DESTDIR/etc/udev/rules.d/
+cp $WORK_DIR/Packaging/Linux/primesense-usb.rules $DESTDIR/etc/udev/rules.d/55-primesense-usb.rules
+
+cd $WORK_DIR/Bin/*Release/
+LIBS=`find .| grep so`
+LIBS2=`find .| grep a$`
+BIN=`find . -type f | grep -v so | grep -v a$`
+
+INSTALL_LIBS="$DESTDIR/usr/lib/openni2/"
+INSTALL_BIN="$DESTDIR/usr/bin/openni2/"
+INSTALL_INCLUDE="$DESTDIR/usr/include/openni2/"
+
+mkdir -p $INSTALL_LIBS
+mkdir -p $INSTALL_BIN
+mkdir -p $INSTALL_INCLUDE
+
+
+echo "Installing libs ..." 
+for lib in $LIBS; do
+	echo "     $lib"
+	cp -a $lib $INSTALL_LIBS
+done
+
+for lib in $LIBS2; do
+	echo "     $lib"
+	cp -a $lib $INSTALL_LIBS
+done
+
+echo "Installing bin ..."
+for bin in $BIN; do
+	echo "     $bin"
+	cp -a $bin $INSTALL_BIN
+done
+
+echo "Installing include ..."
+cd -
+cp -a Include/* $INSTALL_INCLUDE
+
+cp -a $WORK_DIR/Bin/*Release/OpenNI2/ $INSTALL_LIBS
+
+mkdir -p $DESTDIR/etc/ld.so.conf.d/ 
+echo "/usr/lib/openni2/" >  $DESTDIR/etc/ld.so.conf.d/openni2.conf
---
+++ OpenNI2/openni2-2.2.0/.pc/installprocess3/Packaging/Linux/install-jderobot.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+WORK_DIR=`pwd`
+
+cd $WORK_DIR/Bin/*Release/
+LIBS=`find .| grep so`
+LIBS2=`find .| grep a$`
+BIN=`find . -type f | grep -v so | grep -v a$`
+
+INSTALL_LIBS="/usr/lib/openni2/"
+INSTALL_BIN="/usr/bin/openni2/"
+INSTALL_INCLUDE="/usr/include/openni2/"
+
+mkdir -p $INSTALL_LIBS
+mkdir -p $INSTALL_BIN
+mkdir -p $INSTALL_INCLUDE
+
+
+echo "Installing libs ..." 
+for lib in $LIBS; do
+	echo "     $lib"
+	cp -a $lib $INSTALL_LIBS
+done
+
+for lib in $LIBS2; do
+	echo "     $lib"
+	cp -a $lib $INSTALL_LIBS
+done
+
+echo "Installing bin ..."
+for bin in $BIN; do
+	echo "     $bin"
+	cp -a $bin $INSTALL_BIN
+done
+
+echo "Installing include ..."
+cd -
+cp -a Include/* $INSTALL_INCLUDE
+
